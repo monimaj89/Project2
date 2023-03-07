5363ef4eba8969b632402e3bc8b8cf7d19cdf3d6
monimaj89 <mn.majchrowicz@gmail.com>
2023-03-07 19:59:15 +0000

    Added radio buttons and changed map default location.

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,13 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 12,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 51.5085300,
+            lng: -0.1257400
         }
     });
 }
+
+
+
+  
